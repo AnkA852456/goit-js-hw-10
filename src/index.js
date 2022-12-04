2851afd1783556d86714ac90d4ffb80e088ad269
AnkA852456 <108530192+AnkA852456@users.noreply.github.com>
2022-12-04 15:37:00 +0200

    HW10_v1

--- src/index.js
+++ src/index.js
@@ -14,7 +14,9 @@ function onCountryChoice(evt) {
   fetchCountries(chosenCountry)
     .then(onCountryList)
     .catch(error => {
-      Notify.failure('Oops, there is no country with that name');
+      Notify.failure('Oops, there is no country with that name', {
+        timeout: 2000,
+      });
       refs.list.innerHTML = '';
       refs.countryInfo.innerHTML = '';
     });
@@ -23,30 +25,40 @@ function onCountryChoice(evt) {
 function onCountryList(countries) {
   const showTheList = countries
     .map(
-      country => `<li class="item" ><img width="40" height="40"src="${country.flags.svg}" alt="Flag of ${country}"><h1 class="title">${country.name.official}</h1>
+      country => `<li class="item-list" ><img width="70" height="40"src="${country.flags.svg}" alt="Flag of ${country}"><h1 class="title">${country.name.official}</h1>
   </li>`
     )
     .join('');
   refs.list.insertAdjacentHTML('beforeend', showTheList);
 
-  // const numberOfCountries = countries.length();
+  const numberOfCountries = countries.length;
 
-  // if (numberOfCountries === 1) {
-  //   const singleCountry = countries
-  //     .map(
-  //       country => `<li class="item" ><img width="40" height="40"src="${country.flags.svg}" alt="Flag of ${country.name}"><h1 class="title">${country.name.official}</h1>
-  //   <p>${country.capital}</p><p>${country.population}</p><p>${country.languages}</p></li>`
-  //     )
-  //     .join('');
-  //   refs.countryInfo.insertAdjacentHTML('beforeend', singleCountry);
-  //   return;
-  // }
+  if (numberOfCountries === 1) {
+    refs.list.innerHTML = '';
+    const singleCountry = countries
+      .map(
+        country => `<li class="item" ><img width="70" height="40"src="${
+          country.flags.svg
+        }" alt="Flag of ${country.name}"><h1 class="title">${
+          country.name.official
+        }</h1>
+    <p> <b>Capital:</b> ${country.capital}</p><p><b>Population:</b> ${
+          country.population
+        }</p><p><b>Languages:</b> ${Object.values(country.languages)}</p></li>`
+      )
+      .join('');
+    refs.countryInfo.insertAdjacentHTML('beforeend', singleCountry);
+    return;
+  }
 
-  // if (numberOfCountries > 10) {
-  //   Notify.warning(
-  //     'Too many matches found. Please enter a more specific name.'
-  //   );
-  // }
+  if (numberOfCountries > 10) {
+    Notify.warning(
+      'Too many matches found. Please enter a more specific name.',
+      {
+        timeout: 2000,
+      }
+    );
+  }
 
-  // refs.countryInfo.innerHTML = '';
+  refs.countryInfo.innerHTML = '';
 }
